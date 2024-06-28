2ee809ac723626959c51a8aca485d4e4e6632a52
lmhuong711 <lmhuong711@gmail.com>
2024-06-28 11:43:55 +0700

    a-maze-ing: butt--

--- utils/movable.js
+++ utils/movable.js
@@ -10,29 +10,35 @@ function dragElement(ele) {
     pos4 = 0;
   const header = document.getElementById(ele.id + "header");
   header
-    ? isMobile
-      ? (header.ontouchstart = dragMouseDown)
-      : (header.onmousedown = dragMouseDown)
-    : isMobile
-    ? (ele.ontouchstart = dragMouseDown)
+    ? (header.onmousedown = dragMouseDown)
     : (ele.onmousedown = dragMouseDown);
+  // ? isMobile
+  //   ? (header.ontouchstart = dragMouseDown)
+  //   : (header.onmousedown = dragMouseDown)
+  // : isMobile
+  // ? (ele.ontouchstart = dragMouseDown)
+  // : (ele.onmousedown = dragMouseDown);
 
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
     pos3 = e.clientX;
     pos4 = e.clientY;
-    isMobile
-      ? (document.ontouchend = closeDragElement)
-      : (document.onmouseup = closeDragElement);
-    isMobile
-      ? (document.ontouchmove = elementDrag)
-      : (document.onmousemove = elementDrag);
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+
+    // isMobile
+    //   ? (document.ontouchend = closeDragElement)
+    //   : (document.onmouseup = closeDragElement);
+    // isMobile
+    //   ? (document.ontouchmove = elementDrag)
+    //   : (document.onmousemove = elementDrag);
   }
 
   function elementDrag(e) {
     e = e || window.event;
-    isMobile ? null : e.preventDefault();
+    e.preventDefault();
+    // isMobile ? null : e.preventDefault();
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
@@ -42,7 +48,9 @@ function dragElement(ele) {
   }
 
   function closeDragElement() {
-    isMobile ? (document.ontouchend = null) : (document.onmouseup = null);
-    isMobile ? (document.ontouchmove = null) : (document.onmousemove = null);
+    document.onmouseup = null;
+    document.onmousemove = null;
+    // isMobile ? (document.ontouchend = null) : (document.onmouseup = null);
+    // isMobile ? (document.ontouchmove = null) : (document.onmousemove = null);
   }
 }
